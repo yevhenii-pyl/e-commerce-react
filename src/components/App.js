08e7bddafd2f62dc110eee30049fd430fc7530c6
Yevhenii <flyhighthendie@gmail.com>
2020-12-05 20:34:54 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import HomePage from './homepage.component'
+
+const App = () => {
+    return (
+        <div>
+            <HomePage />
+        </div>
+    )
+}
+
+export default App
