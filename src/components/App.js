7542732ab1cbb30fbbf956dc204cafa15f89571b
Yevhenii <flyhighthendie@gmail.com>
2020-12-07 21:42:23 +0200

    added header components and routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 
 import './App.scss'
 
+import Header from './header/header.component'
 import HomePage from '../pages/homepage/homepage.component'
 import ShopPage from '../pages/shop/shop.component'
 
 const App = () => {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route path='/' exact component={ HomePage } />
                 <Route path='/shop' component={ ShopPage } />
