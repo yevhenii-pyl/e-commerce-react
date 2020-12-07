8b5f2d8fa5d730427b140e7a7e805af4e55a6c0f
Yevhenii <flyhighthendie@gmail.com>
2020-12-07 21:27:34 +0200

    added shop page with basic styling and components

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom'
 import './App.scss'
 
 import HomePage from '../pages/homepage/homepage.component'
+import ShopPage from '../pages/shop/shop.component'
 
 const App = () => {
     return (
         <div>
             <Switch>
                 <Route path='/' exact component={ HomePage } />
+                <Route path='/shop' component={ ShopPage } />
             </Switch>
         </div>
     )
---
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import './preview-collection.styles.scss'
+
+import CollectionItem from '../collection-item/collection-item.component'
+
+const PreviewCollection = ({ title, items }) => {
+    return(
+        <div className="preview-collection">
+            <h1 className="title">{ title.toUpperCase() }</h1>
+            <div className="preview">
+                {
+                    items
+                        .filter((item, idx)=> idx < 4)
+                        .map(({ id, ...itemProps}) => {
+                            return(
+                                <CollectionItem key={id} {...itemProps} />
+                                )
+                            })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default PreviewCollection
